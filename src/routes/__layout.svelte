362b401e6f889e773b0332374b0776b6ea10f9b5
erbridge <f@erbridge.co.uk>
2021-08-07 11:50:32 +0200

    Style the text

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,16 @@
+<slot />
+
+<style global>
+	:root {
+		font-family: 'Eczar', serif;
+	}
+
+	h1 {
+		font-family: 'IM Fell English', serif;
+	}
+
+	h1 > div {
+		font-family: 'Eczar', serif;
+		font-size: 1.5em;
+	}
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,7 +2,10 @@
 	<title>dungeonpunk</title>
 </svelte:head>
 
-<h1>dungeonpunk hacks</h1>
+<h1>
+	dungeonpunk
+	<div>hacks</div>
+</h1>
 
 <ul>
 	<li><a href="mortal-wounds">mortal wounds</a></li>
