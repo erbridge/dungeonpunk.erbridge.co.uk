2b1553bff35c8d91474a965c5e9124c16b616e87
erbridge <f@erbridge.co.uk>
2021-08-07 12:14:26 +0200

    Introduce the index

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,11 +2,22 @@
 	<title>dungeonpunk</title>
 </svelte:head>
 
-<h1>
-	dungeonpunk
-	<div>hacks</div>
-</h1>
+<main>
+	<h1>
+		dungeonpunk
+		<div>hacks</div>
+	</h1>
 
-<ul>
-	<li><a href="mortal-wounds">mortal wounds</a></li>
-</ul>
+	<p>
+		These are hacks for <a
+			href="https://acegiak.itch.io/dungeonpunk"
+			target="_blank"
+			rel="noopener noreferrer">dungeonpunk</a
+		> by Ash McAllen, Sidney Icarus, and Melody Watson, a fiction-telling game about exploring a fantasy
+		world and going on adventures.
+	</p>
+
+	<ul>
+		<li><a href="mortal-wounds">mortal wounds</a></li>
+	</ul>
+</main>
